ca34a090e57788b2f2ba2611a04c3574d2333f96
Skulitorr <Skulitor22@gmail.com>
2025-05-31 23:50:16 +0000

    Create App component and start development server

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
+import Dashboard from './routes/Dashboard';
+import Schedule from './routes/Schedule';
+import CallIn from './routes/CallIn';
+import Admin from './routes/Admin';
+
+function App() {
+  return (
+    <Router>
+      <AppProvider>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/callin" element={<CallIn />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AppProvider>
+    </Router>
+  );
+}
+
+export default App;
